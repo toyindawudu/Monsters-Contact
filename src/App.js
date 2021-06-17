6272a7f73ac9752af681ded1b5c403ab2ec70b87
Toyin Dawudu <toyindawudu@gmail.com>
2021-06-17 22:21:17 +0100

    Creating a card component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,32 @@
 import React, { Component }from 'react';
+
+import { CardList } from './components/cardList/cardList';
 import './App.css';
 
 
-class App extends React.Component {
+class App extends Component {
 
   constructor() {
       super();
 
       this.state = {
-        monsters: [
-          {
-            name: 'Frankenstein',
-            age: 45
-          }, 
-          {
-            name: 'Chucky',
-            age: 49
-          },
-          {
-            name: 'Zombie',
-            age: 53
-          }
-        ],
-
+        monsters: []
       };
+    }
+
+    componentDidMount(){
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters:users }));
     }
 
   render(){
 
     return (
       <div className="App">
+      <CardList />
         {
-          this.state.monsters.map(monsters => <h1>{monsters.name}, {monsters.age}</h1>)
+          this.state.monsters.map(monsters => (<h1>{monsters.name}, {monsters.age}</h1>))
         }
       </div>
     );
